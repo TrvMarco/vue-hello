63e9ccacf2c4524f12d78547fbda93570f7178ea
TrvMarco <trevisanello.marco@gmail.com>
2022-05-19 15:36:41 +0200

    aggiunto  immagine

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const app = new Vue({
     el: '#root',
     data: {
-        message: 'Hello World, Finally on Vue!'
+        message: 'Hello World, Finally on Vue!',
+        image: 'https://gamerzsfx.com.br/wp-content/uploads/2020/08/hello-world.jpg',
     }
-})
+});
